5524ecee9cdbede5d79b0df8dba445d173c87a80
Paul K. Lynch <dramatichamster@gmail.com>
2025-10-21 21:45:57 +1100

    Show results list

--- src/Results.tsx
+++ src/Results.tsx
@@ -1,5 +1,6 @@
 // import { useState, useMemo } from 'react'
 import words from 'an-array-of-english-words'
+import './assets/results.css'
 
 import type { FC } from 'react'
 import type { ResultsProps } from './types'
@@ -35,10 +36,6 @@ export const Results: FC<ResultsProps> = ({ values }) => {
     return word.length === longLength
   })
 
-  console.log('shortWords', shortWords.size)
-  console.log('longWords', longWords.length, longWords[0])
-  // console.log('test', longWords[0].substring(fistShortIndex, lastShortIndex + 1))
-
   longWords.forEach(longWord => {
     const subWord = longWord.substring(fistShortIndex, lastShortIndex + 1)
     if (!shortWords.has(subWord)) return
@@ -52,18 +49,30 @@ export const Results: FC<ResultsProps> = ({ values }) => {
     matches[letter][subWord].push(longWord)
   })
 
-  console.log('matches', matches)
-  
+  const entries = Object.entries(matches)
 
-  console.log('render', { longLength, shortLength, fistShortIndex, lastShortIndex })
+  if (entries.length === 0) {
+    // I don't think this can happen
+    return <p>No result?</p>
+  }
+  
   return (
-    <section aria-labelledby="results-title">
+    <section aria-labelledby="results-title" className="results">
       <h2 id="results-title">Results</h2>
-      <ul>
-      {Object.entries(matches)
+      <ul className="results__list">
+      {entries
         .toSorted()
-        .map(([letter]) => (
-          <li>{letter}</li>
+        .map(([letter, item]) => (
+          <li className="results__entry">
+            <h3 className="results__letter">{letter}</h3>
+            <ul className="results__words">
+              {Object.entries(item).map(([short, longList]) => (
+                <li className="results__word">
+                  {short} <span className="results__count">({longList.length})</span>
+                </li>
+              ))}
+            </ul>
+          </li>
         ))
       }
       </ul>
